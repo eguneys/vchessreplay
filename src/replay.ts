43c3d517d2b1dd60867c0db8db726677f1366f6b
Emre Guneyler <eguneys@gmail.com>
2022-06-04 01:21:41 +0300

    tree flat design

--- src/replay.ts
+++ src/replay.ts
@@ -1,7 +1,42 @@
+import { createSignal } from 'solid-js'
+import { read, write, owrite } from './play'
+
 export class Replay {
 
+  get moves() {
+    return this.a_moves.moves
+  }
+
+  set moves(moves: Array<Moves>) {
+    this.a_moves.moves = moves
+  }
+
+  constructor() {
+    this.a_moves = make_moves()
 
 
+    this.moves = [
+      'path', 
+      'path', 
+      'path',
+      'path',
+      'path', 
+      'path',
+      'path {A little comment}', ]
+  }
+}
 
 
+const make_moves = () => {
+  let _moves = createSignal([])
+
+
+  return {
+    get moves() {
+      return read(_moves)
+    },
+    set moves(moves: Array<Move>) {
+      owrite(_moves, moves)
+    }
+  }
 }
--- src/view.tsx
+++ src/view.tsx
@@ -1,7 +1,23 @@
 
 const App  = replay => props => {
   return (<>
+   <vchessreplay>
+     <moves>
+       <For each={replay.moves}>{ move =>
+         <Move move={move}/>
+       }</For>
+     </moves>
+   </vchessreplay> 
       </>)
+}
+
+const Move = props => {
+  return (<move>
+      <Show when={props.move.index}>{ index =>
+        <index>{index}</index>
+      }</Show>
+      {props.move.move}
+      </move>)
 }
 
 export default App
