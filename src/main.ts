d70cb22d793ba128c6460f9ecbdce0542db1394d
Emre Guneyler <eguneys@gmail.com>
2022-06-03 23:31:42 +0300

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import './index.css'
+import { render } from 'solid-js/web'
+
+import App from './view'
+
+import { Replay } from './replay'
+
+export default function VChessreplay(element: HTMLElement, options = {}) {
+
+  let replay = new Replay()
+  render(App(replay), element)
+
+  return {
+  }
+}
---
+++ src/view.tsx
@@ -0,0 +1,7 @@
+
+const App  = replay => props => {
+  return (<>
+      </>)
+}
+
+export default App
