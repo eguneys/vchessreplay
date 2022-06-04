b0e870f9cc678f96a8ea6d3f7c213416c9a8d6a0
Emre Guneyler <eguneys@gmail.com>
2022-06-05 01:00:55 +0300

    hover path

--- src/replay.ts
+++ src/replay.ts
@@ -16,32 +16,61 @@ export class Replay {
 
 
     this.moves = [
-      'pa',
-      'path', 
-      'path', 
-      'path',
-      'path',
-      'path', 
-      'path',
-      'path {A little comment}', ]
+      'd4 d4',
+      'd4d5 d5',
+      'd4d5f4 Bf4',
+      'd4d5f4c5 c5',
+      'd4d5f4c5e3 e3',
+      'd4d5f4c5e3d4 cxd4',
+      'd4d5f4c5e3d4d4 exd4',
+      'd4d5f4c5e3b6 Qb6',
+      'd4d5f4c5e3b6c3 Nc3',
+      'd4d5f4c5e3b6c3e6 e6',
+      'd4d5f4c5e3b6c3e6f3 Nf3',
+      'd4d5f4c5e3b6c3e6f3e7 Be7 { Hello world }',
+      'd4d5f4c5e3b6c3e6f3c4 c4',
+
+      'd4d5f4c5e3b6c3e6f3e7a5 a5 { What s up ok ok ok ook }',
+      'd4d5f4c5e3b6c3e6f3e7a5d8 Qd8',
+
+
+      'd4d5f4c5e3b6c3e6f3c4b3 b3',
+      'd4d5f4c5e3b6c3e6f3c4b3b5 b5',
+
+      'd4d5f4c5e3b6c3e6f3c4b3b5b1 Rb1',
+      'd4d5f4c5e3b6c3e6f3c4b3b5b1a5 Qa5',
+
+      'd4d5f4c5e3b6c3e6f3c4b3b5b1a5b7 Rxb7',
+      'd4d5f4c5e3b6c3e6f3c4b3b5b1a5b7c3 Qxc3',
+
+      'd4d5f4c5e3b6c3e6f3c4b3b5b1a5c4 Bxc4',
+      'd4d5f4c5e3b6c3e6f3c4b3b5b1a5c4c7 Qxc7',
+
+
+      'd4d5f4c5e3b6c3e6f3c4b3b5b1d7 Qd7',
+      'd4d5f4c5e3b6c3e6f3c4b3b5b1d7e5 Ne5',
+    ]
   }
 }
 
 const make_move = (_path: string) => {
-  let _comments = _path.match(/([^ ]*) \{([^\}]*)\}/)
+  let _comments = _path.match(/([^ ]*) ([^ ]*) \{([^\}]*)\}/)
   let comments,
-    path
+    path,
+    move
   if (_comments) {
     path = _comments[1]
-    comments = _comments[2]
+    move = _comments[2]
+    comments = _comments[3]
   } else {
-    path = _path
+    [path, move] = _path.split(' ')
   }
 
-  let base = path.slice(0, -2),
-  move = path.slice(-2)
+  let base = path.length === 2 ? '' : path.slice(0, -2)
 
+  let ply = path.length / 2
   return {
+    ply,
     path,
     base,
     move,
@@ -55,16 +84,47 @@ const make_moves = () => {
   let a_moves = createMemo(mapArray(_moves[0], make_move))
 
   function make_nodes(path: string) {
-    return createMemo(() => 
-      a_moves().filter(_ => _.base === path).map(a_move => ({
-        a_move,
-        m_cs: make_nodes(a_move.path)
-      })))
+    return createMemo(() =>  {
+      let children = a_moves().filter(_ => _.base === path)
+      
+      let is_branch = children.length > 1
+
+      return children.map(a_move => {
+        let m_cs = make_nodes(a_move.path)
+
+        let cs = m_cs()
+        let main_children_and_rest
+        if (cs.length > 0) {
+          main_children_and_rest = [
+            cs[0],
+            cs.slice(1)
+          ]
+        }
+
+        let i_continue = a_move.ply % 2 === 0
+        let index = Math.ceil(a_move.ply / 2) + (i_continue ? '...' : '.')
+
+        let show_index = is_branch || !i_continue
+
+        return {
+          a_move,
+          index,
+          show_index,
+          main_children_and_rest
+        }
+      })
+    })
   }
 
   let m_tree = make_nodes('')
 
   return {
+    hover_path(path: string) {
+      console.log(path)
+    },
+    hover_off() {
+      console.log('off')
+    },
     get tree() {
       return m_tree()
     },
--- src/view.tsx
+++ src/view.tsx
@@ -1,14 +1,24 @@
 
+function non_empty_array<A>(arr: Array<A>) {
+  if (Array.isArray(arr) && arr.length > 0) {
+    return arr
+  }
+}
+
 const App  = replay => props => {
+
+  let on_hover = (path: string) => {
+    replay.a_moves.hover_path(path)
+  }
+
+
   return (<>
    <vchessreplay>
      <moves>
        <For each={replay.tree}>{ node =>
          <>
-         <Move move={{}}/>
-         <For each={console.log(node.m_cs())||node.m_cs()}>{ child =>
-           <Move move={{}}/>
-         }</For>
+           <Move on_hover={on_hover} {...node}/>
+           <MainChildrenAndRest on_hover={on_hover} mcr={node.main_children_and_rest}/>
          </>
        }</For>
      </moves>
@@ -16,13 +26,42 @@ const App  = replay => props => {
       </>)
 }
 
+const MainChildrenAndRest = props => {
+  if (!props.mcr) {
+    return (<></>)
+  }
+  return (<>
+    <Show when={props.mcr[1].length}
+    fallback = {<>
+      <Move on_hover={props.on_hover} {...props.mcr[0]}/>
+      <MainChildrenAndRest on_hover={props.on_hover} mcr={props.mcr[0].main_children_and_rest}/>
+      </>
+    }>
+      <lines>
+        <line->
+          <Move on_hover={props.on_hover} {...props.mcr[0]}/>
+          <MainChildrenAndRest on_hover={props.on_hover} mcr={props.mcr[0].main_children_and_rest}/>
+        </line->
+        <For each={props.mcr[1]}>{ child =>
+         <line->
+           <Move on_hover={props.on_hover} {...child}/>
+           <MainChildrenAndRest on_hover={props.on_hover} mcr={child.main_children_and_rest}/>
+         </line->
+        }</For>
+      </lines> 
+    </Show>
+     </>)
+}
+
 const Move = props => {
-  return (<move>
-      <Show when={props.move.index}>{ index =>
-        <index>{index}</index>
-      }</Show>
-      {props.move.move}
-      </move>)
+  return (<><move onMouseOver={_ => props.on_hover(props.a_move.path)}>
+      <Show when={props.show_index}>
+        <index>{props.index}</index>
+      </Show>
+      {props.a_move.move}
+      </move>
+      <comment>{props.a_move.comments}</comment>
+      </>)
 }
 
 export default App
